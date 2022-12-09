f604d731db65bde535638412565e3ec27595687d
Om3ga <carriermatthew25@outlook.com>
2022-12-09 15:31:24 -0500

    Add project files.

---
+++ BSATroop829/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BSATroop829.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
