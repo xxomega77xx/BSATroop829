d1d94d601d35102c125d62b3bb9157cb1b3c7d84
Om3ga <carriermatthew25@outlook.com>
2022-12-18 14:12:04 -0500

    Added more colums based on activities coord request Fixed some CSS issues

---
+++ BSATroop829/Data/Migrations/20221218190246_AddDataColumnsforSUmmerCamp.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BSATroop829.Data.Migrations
+{
+    public partial class AddDataColumnsforSUmmerCamp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TimeSlot",
+                table: "SummerCamp",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Priority",
+                table: "SummerCamp",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MeritBadge",
+                table: "SummerCamp",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "SummerCamp",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rank",
+                table: "SummerCamp",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "SummerCamp");
+
+            migrationBuilder.DropColumn(
+                name: "Rank",
+                table: "SummerCamp");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TimeSlot",
+                table: "SummerCamp",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Priority",
+                table: "SummerCamp",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MeritBadge",
+                table: "SummerCamp",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- BSATroop829/Models/SummerCampViewModel.cs
+++ BSATroop829/Models/SummerCampViewModel.cs
@@ -8,10 +8,18 @@ namespace BSATroop829.Models
         public int Id { get; set; }
         [Required]
         public string? ScoutName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string? Rank { get; set; }
+        [Required]
         public string? MeritBadge { get; set; }
+        [Required]
         public string? TimeSlot { get; set; }
+        [Required]
+        public string? Priority { get; set; }
         public bool EagleRequired  { get; set; }
         public string? Prerequisites { get; set; }
-        public string? Priority { get; set; }
+        
     }
 }
