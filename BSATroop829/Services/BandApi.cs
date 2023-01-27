c6538d684d7ce43706bbaefb3fb05a24e27b3248
Matthew Carrier <matthew.s.carrier.civ@army.mil>
2023-01-27 18:46:09 -0500

    Band API for getting posts implemented

---
+++ BSATroop829/Services/BandApi.cs
@@ -0,0 +1,56 @@
+﻿using BSATroop829.Services.BandApiClasses;
+using Newtonsoft.Json;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+namespace BSATroop829.Services
+{
+    public class BandApi
+    {
+        public static List<Item> GetPosts()
+        {
+            HttpClient client = new HttpClient();
+            //client.BaseAddress = new Uri("https://openapi.band.us/v2.1/bands/");
+            client.BaseAddress = new Uri("https://openapi.band.us/v2/band/posts/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            // Send the request to retrieve posts
+            //HttpResponseMessage response = client.GetAsync("?access_token=ZQAAAUI--HAT4ar2TNQVdncePU6kt7_qcd6cTFjakKfZo7rGAD3CZtSau1NHhCfbvVYMlDzGW6UMJTlDTfsEUjZ8NBYKA5JGRF4rWFiecPTgyhCy").Result;
+            //client.DefaultRequestHeaders.Add("", "");
+            var response = client.GetAsync("?band_key=AADvhG7pafhVFqr6UebSPZHm&access_token=ZQAAAUI--HAT4ar2TNQVdncePU6kt7_qcd6cTFjakKfZo7rGAD3CZtSau1NHhCfbvVYMlDzGW6UMJTlDTfsEUjZ8NBYKA5JGRF4rWFiecPTgyhCy&local=en_US").Result;
+
+            // Read the response and display the posts
+            string jsonString = response.Content.ReadAsStringAsync().Result;
+            Root JsonClass = JsonConvert.DeserializeObject<Root>(jsonString);
+            var posts = JsonClass.result_data.items;
+            return posts;
+        }
+        public static DateTime? GetDateTimeFromInt(long? dateAsLong, bool hasTime = true)
+        {
+            if (dateAsLong.HasValue && dateAsLong > 0)
+            {
+                if (hasTime)
+                {
+                    // sometimes input is 14 digit and sometimes 16
+                    var numberOfDigits = (int)Math.Floor(Math.Log10(dateAsLong.Value) + 1);
+
+                    if (numberOfDigits > 14)
+                    {
+                        dateAsLong /= (int)Math.Pow(10, (numberOfDigits - 14));
+                    }
+                }
+
+                if (DateTime.TryParseExact(dateAsLong.ToString(), hasTime ? "yyyyMMddHHmmss" : "yyyyMMdd",
+                                          CultureInfo.InvariantCulture,
+                                          DateTimeStyles.None, out DateTime dt))
+                {
+                    return dt;
+                }
+            }
+
+            return null;
+        }
+    }
+}
+
---
+++ BSATroop829/Services/BandApiClasses/BandApiClasses.cs
@@ -0,0 +1,77 @@
+﻿namespace BSATroop829.Services.BandApiClasses
+{
+    public class Author
+    {
+        public string name { get; set; }
+        public string profile_image_url { get; set; }
+        public string description { get; set; }
+        public string role { get; set; }
+        public string member_type { get; set; }
+        public bool member_certified { get; set; }
+        public bool me { get; set; }
+        public bool is_muted { get; set; }
+        public object created_at { get; set; }
+        public string user_key { get; set; }
+    }
+
+    public class Item
+    {
+        public Author author { get; set; }
+        public string post_key { get; set; }
+        public string content { get; set; }
+        public int comment_count { get; set; }
+        public long created_at { get; set; }
+        public List<Photo> photos { get; set; }
+        public int emotion_count { get; set; }
+        public List<LatestComment> latest_comments { get; set; }
+        public string band_key { get; set; }
+    }
+
+    public class LatestComment
+    {
+        public string body { get; set; }
+        public object created_at { get; set; }
+        public Author author { get; set; }
+    }
+
+    public class NextParams
+    {
+        public string access_token { get; set; }
+        public string band_key { get; set; }
+        public string limit { get; set; }
+        public string after { get; set; }
+        public string local { get; set; }
+    }
+
+    public class Paging
+    {
+        public object previous_params { get; set; }
+        public NextParams next_params { get; set; }
+    }
+
+    public class Photo
+    {
+        public int height { get; set; }
+        public int width { get; set; }
+        public object created_at { get; set; }
+        public int comment_count { get; set; }
+        public string url { get; set; }
+        public int emotion_count { get; set; }
+        public string photo_key { get; set; }
+        public Author author { get; set; }
+        public bool is_video_thumbnail { get; set; }
+        public object photo_album_key { get; set; }
+    }
+
+    public class ResultData
+    {
+        public List<Item> items { get; set; }
+        public Paging paging { get; set; }
+    }
+
+    public class Root
+    {
+        public int result_code { get; set; }
+        public ResultData result_data { get; set; }
+    }
+}
